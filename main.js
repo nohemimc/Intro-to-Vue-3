ed7bdfd915dbaa8ad1e6e9fea21460aa32971988
Erika Miranda <nohemileonla@gmail.com>
2023-01-03 12:41:26 -0600

    Poyecto de introducción a VueJS

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  data() {
+    return {
+      cart: [],
+      premium: false,
+      active: true,
+    };
+  },
+  methods: {
+    updateCart(id) {
+      this.cart.push(id);
+    },
+    deleteCart(id) {
+      //    this.cart.length >=1 ? this.cart.pop(id) : ''
+      const index = this.cart.indexOf(id);
+      if (index > -1) {
+        this.cart.splice(index, 1);
+      }
+      else {
+        this.active=false
+      }
+    },
+  },
+});
